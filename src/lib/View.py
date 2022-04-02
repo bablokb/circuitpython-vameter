050b474110c8d4c5d04607586bb4ed513f41bbef
Bernhard Bablok <bablokb@gmx.de>
2022-04-02 09:51:30 +0200

    make all methods to noops if diplay is None

--- src/lib/View.py
+++ src/lib/View.py
@@ -26,31 +26,35 @@ class View:
     """ constructor """
 
     self._display = display
-    self._group   = displayio.Group()
-    offset = border + 2 if border else 0
-    self._pos_map = {
-      'NW': ((0.0,0.0),(offset,              offset)),
-      'NE': ((1.0,0.0),(display.width-offset,offset)),
-      'W':  ((0.0,0.5),(offset,              display.height/2)),
-      'E':  ((1.0,0.5),(display.width-offset,display.height/2)),
-      'SW': ((0.0,1.0),(offset,              display.height-offset)),
-      'SE': ((1.0,1.0),(display.width-offset,display.height-offset)),
-      }
-    if border:
-      rect = Rect(0,0,display.width,display.height,
-                  fill=None,outline=View.FG_COLOR,stroke=border)
-      self._group.append(rect)
+    if display:
+      self._group   = displayio.Group()
+      offset = border + 2 if border else 0
+      self._pos_map = {
+        'NW': ((0.0,0.0),(offset,              offset)),
+        'NE': ((1.0,0.0),(display.width-offset,offset)),
+        'W':  ((0.0,0.5),(offset,              display.height/2)),
+        'E':  ((1.0,0.5),(display.width-offset,display.height/2)),
+        'SW': ((0.0,1.0),(offset,              display.height-offset)),
+        'SE': ((1.0,1.0),(display.width-offset,display.height-offset)),
+        }
+      if border:
+        rect = Rect(0,0,display.width,display.height,
+                    fill=None,outline=View.FG_COLOR,stroke=border)
+        self._group.append(rect)
 
   # --- add a text   ---------------------------------------------------------
 
   def add(self,text,pos,font):
     """ create and add a label with given text, pos, font """
 
-    t = label.Label(font,text=text,color=View.FG_COLOR,
-                    anchor_point=self._pos_map[pos][0])
-    t.anchored_position = self._pos_map[pos][1]
-    self._group.append(t)
-    return t
+    if self._display:
+      t = label.Label(font,text=text,color=View.FG_COLOR,
+                      anchor_point=self._pos_map[pos][0])
+      t.anchored_position = self._pos_map[pos][1]
+      self._group.append(t)
+      return t
+    else:
+      return None
 
   # --- format a float   -----------------------------------------------------
 
@@ -87,16 +91,18 @@ class ValuesView(View):
     """ constructor """
 
     super(ValuesView,self).__init__(display,border)
-    self._value_V = self.add('0.00V','NE',View.FONT_L)
-    self._value_A = self.add('0.00mA','SE',View.FONT_L)
+    if self._display:
+      self._value_V = self.add('0.00V','NE',View.FONT_L)
+      self._value_A = self.add('0.00mA','SE',View.FONT_L)
 
   # --- set values   ---------------------------------------------------------
 
   def set_values(self,v,a):
     """ set values for voltage and current """
 
-    self._value_V.text = self.format(v,'V')
-    self._value_A.text = self.format(a,'mA')
+    if self._display:
+      self._value_V.text = self.format(v,'V')
+      self._value_A.text = self.format(a,'mA')
 
 # ----------------------------------------------------------------------------
 # --- View for results   -----------------------------------------------------
@@ -111,23 +117,25 @@ class ResultView(View):
     super(ResultView,self).__init__(display,border)
     self._unit = unit
 
-    self._label_min = self.add('min:','NW',View.FONT_S)
-    self._value_min = self.add('0.00','NE',View.FONT_S)
+    if self._display:
+      self._label_min = self.add('min:','NW',View.FONT_S)
+      self._value_min = self.add('0.00','NE',View.FONT_S)
 
-    self._label_mean = self.add('mean:','W',View.FONT_S)
-    self._value_mean = self.add('0.00','E',View.FONT_S)
+      self._label_mean = self.add('mean:','W',View.FONT_S)
+      self._value_mean = self.add('0.00','E',View.FONT_S)
 
-    self._label_max = self.add('max:','SW',View.FONT_S)
-    self._value_max = self.add('0.00','SE',View.FONT_S)
+      self._label_max = self.add('max:','SW',View.FONT_S)
+      self._value_max = self.add('0.00','SE',View.FONT_S)
 
   # --- set values   ---------------------------------------------------------
 
   def set_values(self,min,mean,max):
     """ set values for voltage and current """
 
-    self._value_min.text  = self.format(min,self._unit)
-    self._value_mean.text = self.format(mean,self._unit)
-    self._value_max.text  = self.format(max,self._unit)
+    if self._display:
+      self._value_min.text  = self.format(min,self._unit)
+      self._value_mean.text = self.format(mean,self._unit)
+      self._value_max.text  = self.format(max,self._unit)
 
 # ----------------------------------------------------------------------------
 # --- View for configurations   ----------------------------------------------
@@ -142,12 +150,14 @@ class ConfigView(View):
     super(ConfigView,self).__init__(display,border)
     self._unit = unit
 
-    self._header = self.add(header,'NW',View.FONT_S)
-    self._value  = self.add(' ','SE',View.FONT_L)
+    if self._display:
+      self._header = self.add(header,'NW',View.FONT_S)
+      self._value  = self.add(' ','SE',View.FONT_L)
 
   # --- set values   ---------------------------------------------------------
 
   def set_value(self,value):
     """ set value for config-item """
 
-    self._value.text = self.format(value,self._unit)
+    if self._display:
+      self._value.text = self.format(value,self._unit)
