b10304b7c23ff6ce6d88afb6b801df28872e5c47
Bernhard Bablok <bablokb@gmx.de>
2022-04-04 19:17:31 +0200

    add KeyEventProvider and fix i2c-in-use error

--- src/main.py
+++ src/main.py
@@ -20,6 +20,7 @@ from ConfigState import ConfigState
 from ActiveState import ActiveState
 
 from FakeDataProvider import DataProvider
+from Touchpad import KeyEventProvider
 
 # --- constants   ------------------------------------------------------------
 
@@ -54,7 +55,12 @@ class VAMeter:
   def __init__(self):
     """ constructor """
 
-    self.display = self._get_display()
+    if not hasattr(board,'DISPLAY'):
+      displayio.release_displays()
+
+    i2c = busio.I2C(sda=PIN_SDA,scl=PIN_SCL)
+
+    self.display = self._get_display(i2c)
     self.border  = OLED_BORDER
 
     self.settings = ValueHolder()
@@ -65,21 +71,21 @@ class VAMeter:
     self.data_provider = DataProvider(self.settings)
     self.results = [[0,0,0] for i in range(self.data_provider.get_dim())]
 
+    self.key_events = KeyEventProvider(i2c,self.settings)
+
     self._ready  = ReadyState(self)
     self._active = ActiveState(self)
     self._config = ConfigState(self)
 
   # --- initialize display   -------------------------------------------------
 
-  def _get_display(self):
+  def _get_display(self,i2c):
     """ initialize hardware """
 
     if hasattr(board,'DISPLAY') and board.DISPLAY:
       return board.DISPLAY
     else:
-      displayio.release_displays()
       try:
-        i2c = busio.I2C(sda=PIN_SDA,scl=PIN_SCL)
         display_bus = displayio.I2CDisplay(i2c, device_address=OLED_ADDR)
         return adafruit_displayio_ssd1306.SSD1306(display_bus,
                                                   width=OLED_WIDTH,
@@ -92,7 +98,7 @@ class VAMeter:
   def run(self):
     """ main loop """
 
-    self._config.run()
+    #self._config.run()
     while True:
       next_state = self._ready.run(self._active,self._config)
       next_state.run()
