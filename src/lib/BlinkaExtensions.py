c2b92cdb6ae2bc7014e69248e487d4b2ae29c88f
Bernhard Bablok <bablokb@gmx.de>
2022-05-28 11:51:57 +0200

    initial commit

---
+++ src/lib/BlinkaExtensions.py
@@ -0,0 +1,51 @@
+# ----------------------------------------------------------------------------
+# BlinkaExtensions.py: extensions when running on a SBC
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/circuitpython-vameter
+#
+# ----------------------------------------------------------------------------
+
+from argparse import ArgumentParser
+
+# --- update settings from cmdline-args   ------------------------------------
+
+def update_settings(settings):
+  """ update settings from cmdline arguments """
+
+  parser = _get_parser(settings)
+  parser.parse_args(namespace=settings)
+
+# --- create parser   --------------------------------------------------------
+
+def _get_parser(settings):
+  """ create parser """
+
+  parser = ArgumentParser(prog="cp-vameter",
+                          add_help=False,
+                          description='cp-vameter - sample sensor-data')
+
+  parser.add_argument('-i', '--interval', metavar='interval',
+      default=settings.interval, type=int,
+      help='sampling interval')
+  parser.add_argument('-s', '--scale', metavar='scale',
+      default=settings.tm_scale, type=str, dest='tm_scale',
+      help='time-scale (ms or s)')
+  parser.add_argument('-o', '--oversample', metavar='oversample',
+      default=settings.oversample, type=int,
+      help='oversampling')
+  parser.add_argument('-D', '--duration', metavar='duration',
+      default=settings.duration, type=int,
+      help='duration of measurement')
+  parser.add_argument('-u', '--update', metavar='update',
+      default=settings.update, type=int,
+      help='screen update interval')
+  parser.add_argument('-n', '--no-plots', action='store_false',
+      dest='plots', default=settings.plots,
+      help="disable plots")
+  parser.add_argument('-h', '--help', action='help',
+      help='print this help')
+
+  return parser
