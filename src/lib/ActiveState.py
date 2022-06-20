352aba955481d31a2a613b3bc26eb9a6c83e4ea2
Bernhard Bablok <bablokb@gmx.de>
2022-06-20 15:33:04 +0200

    implemented asyncio-support

--- src/lib/ActiveState.py
+++ src/lib/ActiveState.py
@@ -10,6 +10,7 @@
 
 import time
 import sys
+import asyncio
 from View import ValuesView, PlotView
 from Data import DataAggregator
 
@@ -29,7 +30,12 @@ class ActiveState:
     else:
       self._tm_scale = 1
     self._dim      = app.data_provider.get_dim()
+
+    # global stop for all tasks
+    self._stop     = False
+
     if self._app.display:
+      self._cur_view = 0
       self._views = [ValuesView(app.display,app.border,
                                  app.data_provider.get_units()),
                       ValuesView(app.display,app.border,['s','s'])]  # elapsed
@@ -53,10 +59,80 @@ class ActiveState:
     return (time.monotonic(),
             [d_sum[i]/self._settings.oversample for i in range(self._dim)])
 
+  # --- check for key-press   ------------------------------------------------
+
+  async def _check_key(self):
+    """ check for key and save it """
+
+    if not self._app.key_events:
+      return
+
+    while not self._stop:
+      key = self._app.key_events.is_key_pressed(
+                                           self._app.key_events.KEYMAP_ACTIVE)
+      if key == 'TOGGLE' and self._app.display:
+        # switch to next view
+        self._cur_view = (self._cur_view+1) % len(self._views)
+      elif key == 'STOP':
+        self._stop = True
+        return
+      await asyncio.sleep(0)
+
+  # --- update views   -------------------------------------------------------
+
+  def _update_views(self,data_v):
+    """ update views """
+
+    if self._app.display and self._settings.update:
+      if self._settings.plots:
+        # update plots
+        #s =  time.monotonic()
+        for i,value in enumerate(data_v):
+          self._views[2+i].set_values([value])
+        #print("#display plots: %f" % (time.monotonic()-s))
+      #s =  time.monotonic()
+      if self._cur_view == 0:
+        # measurement values
+        self._views[self._cur_view].set_values(
+          data_v,time.monotonic()-self._start_t)
+      elif self._cur_view == 1:
+        # elapsed time
+        self._views[self._cur_view].set_values(
+          [time.monotonic()-self._start_t,self._settings.duration],-1)
+      #print("#display values: %f" % (time.monotonic()-s))
+
+  # --- show current views   -------------------------------------------------
+
+  async def _show_view(self):
+    """ show current view """
+
+    if not (self._app.display and self._settings.update):
+      return
+
+    while not self._stop:
+      await asyncio.sleep(self._settings.update*self._tm_scale)
+
+      #s =  time.monotonic()
+      self._views[self._cur_view].show()                # show current view
+      if not self._app.key_events:                      # auto toggle view
+        self._cur_view = (self._cur_view+1) % len(self._views)
+      #print("#display show: %f" % (time.monotonic()-s))
+
   # --- loop during ready-state   --------------------------------------------
 
   def run(self):
     """ main-loop during active-state """
+
+    self._stop     = False
+    asyncio.run(self._run())
+
+  # --- loop during ready-state   --------------------------------------------
+
+  async def _run(self):
+    """ main-loop during active-state """
+
+    key_task  = asyncio.create_task(self._check_key())
+    view_task = asyncio.create_task(self._show_view())
 
     self._logger.log_settings()
     m_data = DataAggregator(self._dim)
@@ -83,79 +159,42 @@ class ActiveState:
       end_t = sys.maxsize
 
     data_t0 = 0                                      # timestamp before last sample
-    stop    = False                                  # global stop
     int_t   = self._settings.interval*self._tm_scale # interval time in sec
-    start_t = time.monotonic()                       # timestamp of start
+    self._start_t = time.monotonic()                 # timestamp of start
     samples = 0                                      # total number of samples
 
     # sample until manual stop or until end of duration
-    while not stop and time.monotonic() < end_t:
+    while not self._stop and time.monotonic() < end_t:
 
-      # calc next screen update
-      if self._app.display and self._settings.update:
-        display_next = time.monotonic() + self._settings.update*self._tm_scale
-      else:
-        display_next = end_t
-      # sample until screen-update is necessary
-      while not stop and time.monotonic() < display_next:
+      # sleep until next sampling interval starts (int_t minus overhead)
+      if data_t0 > 0:
+        await asyncio.sleep(max(int_t - (time.monotonic()-data_t0),0))
 
-        # sleep until next sampling interval starts (int_t minus overhead)
-        if data_t0 > 0:
-          time.sleep(max(int_t - (time.monotonic()-data_t0),0))
+      # get, log and save data
+      try:
+        data_t0 = time.monotonic()
+        data_t,data_v = self._get_data()
+        self._logger.log_values(data_t,data_v)
+        #s =  time.monotonic()
+        m_data.add(data_v)
+        #print("#add: %f" % (time.monotonic()-s))
+        samples += 1
+      except StopIteration:
+        self._stop = True
+        break
 
-        # get, log and save data
-        try:
-          data_t0 = time.monotonic()
-          data_t,data_v = self._get_data()
-          self._logger.log_values(data_t,data_v)
-          #s =  time.monotonic()
-          m_data.add(data_v)
-          #print("#add: %f" % (time.monotonic()-s))
-          samples += 1
-        except StopIteration:
-          stop = True
-          break
-
-        # check and process key
-        if self._app.key_events:
-          key = self._app.key_events.is_key_pressed(
-                                           self._app.key_events.KEYMAP_ACTIVE)
-          if key == 'TOGGLE' and self._app.display:
-            # switch to next view
-            c_view = (c_view+1) % len(self._views)
-          elif key == 'STOP':
-            stop = True
-            break
-      if stop:
+      if self._stop:
         break
 
       # update display with current values
-      if self._app.display and self._settings.update:
-        if self._settings.plots:
-          # update plots
-          #s =  time.monotonic()
-          for i,value in enumerate(data_v):
-            self._views[2+i].set_values([value])
-          #print("#display plots: %f" % (time.monotonic()-s))
-        #s =  time.monotonic()
-        if c_view == 0:
-          # measurement values
-          self._views[c_view].set_values(data_v,time.monotonic()-start_t)
-        elif c_view == 1:
-          # elapsed time
-          self._views[c_view].set_values([time.monotonic()-start_t,
-                                           self._settings.duration],-1)
-        #print("#display values: %f" % (time.monotonic()-s))
-        #s =  time.monotonic()
-        self._views[c_view].show()
-        if not self._app.key_events:
-          # auto toggle view
-          c_view = (c_view+1) % len(self._views)
-        #print("#display show: %f" % (time.monotonic()-s))
+      self._update_views(data_v)
 
     # that's it, save and log results
-    self._app.results.time    = data_t - start_t
+    self._app.results.time    = data_t - self._start_t
     self._app.results.samples = samples
     self._app.results.values  = m_data.get()
 
     self._logger.log_summary(samples)
+
+    self._stop = True
+    await asyncio.gather(key_task,view_task)
